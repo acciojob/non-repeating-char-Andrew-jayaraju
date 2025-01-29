8b967c9fae1c09fc116515df1adf1b7cda2e0765
Andrew Vivek <andrew.jayaraju@gmail.com>
2025-01-29 22:33:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=1; i<str.length-1; i++){
+        if(str.charAt(i) != str.charAt(i-1) && str.charAt(i) != str.charAt(i+1)){
+            return str.charAt(i);
+        }
+    }
+    return str.charAt(str.length);
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 

4ca44463d15a9df0752f163cb9918189cb118517
Andrew Vivek <andrew.jayaraju@gmail.com>
2025-01-29 22:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-	for (let i = 0; i < str.length; i++) {
-        // Check if the current character is not repeated
-        if (str.charAt(i) !== str.charAt(i - 1) && str.charAt(i) !== str.charAt(i + 1)) {
+    const charCount = {};
+
+    // Count the occurrences of each character in the string
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charAt(i);
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    // Find the first character with a count of 1 (non-repeated character)
+    for (let i = 0; i < str.length; i++) {
+        if (charCount[str.charAt(i)] === 1) {
             return str.charAt(i);
         }
     }
+
     // If no non-repeated character is found, return null or another appropriate value
     return null;
 }
